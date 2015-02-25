b8c61ebb6510c00f8f6dfafb65d3b943b89dcd46
Gregor MacLennan <gmaclennan@digital-democracy.org>
2015-02-25 15:30:07 -0800

    Add option to parse form to geojson

--- xform-to-json.js
+++ xform-to-json.js
@@ -43,18 +43,51 @@ function coerceProperties(value) {
   }
 }
 
+// List of sorted keynames on a location object
+// (used for identifying valid location objects)
+var locationKeys = Object.keys(parseGeopoint('0 0 0 0')).sort();
+
+// Returns true for a location field
+function isLocation(value) {
+  if (!(value instanceof Object)) return false;
+  var keys = Object.keys(value).sort();
+  return _.isEqual(keys, locationKeys);
+}
+
+// Setting geojson to true will output geojson if a location field is found
+// If locationField is null or undefined, it will abritrarily pick the first
+// location field it finds in the form (ODK forms can contain multiple locations)
+// You can specify locationField with the name of the field you want to use
+// for the geojson coordinates. If geojson is false, locationField is ignored.
+var defaults = {
+  geojson: false,
+  locationField: null
+};
+
 // Parses an xform submission from ODK collect into a JSON string, cleaning up
 // the data and adding metadata
-module.exports = function (data, meta, callback) {
-  meta = meta || {};
+module.exports = function (data, options, callback) {
+  if (!callback) {
+    callback = options;
+    options = {};
+  }
+
+  options = _.defaults(options, defaults);
+
+  var meta = options.meta || {};
 
   parseXmlString(data, function (err, json) {
     var form;
     var formKey;
+    var geojson = {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point"
+      }
+    };
 
     if (err) {
       console.error('error parsing XML string', err);
-
       return callback(err);
     }
 
@@ -84,7 +117,22 @@ module.exports = function (data, meta, callback) {
 
     // Turn boolean strings, floats, and integers into native objects and turn
     // the geopoint into something more readable
-    form = traverse(form).forEach(coerceProperties);
+    form = traverse(form).forEach(function(value) {
+      coerceProperties.call(this, value);
+
+      if (options.geojson) {
+        var isSelectedLocationField = (options.locationField === this.key && isLocation(this.node));
+        var isFirstLocationField = (!geojson.geometry.coordinates && isLocation(this.node));
+        if (isSelectedLocationField || isFirstLocationField) {
+          geojson.geometry.coordinates = [this.node.longitude, this.node.latitude];
+        }
+      }
+    });
+
+    if (options.geojson && geojson.geometry.coordinates) {
+      geojson.properties = form;
+      form = geojson;
+    }
 
     callback(null, form);
   });
